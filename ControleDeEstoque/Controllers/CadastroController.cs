232abe7153706a7060f4b5bf9dc17bc824503c37
Rafael <rafaeltavares462@gmail.com>
2022-09-27 06:14:59 -0300

    Adicionar arquivos de projeto.

---
+++ ControleDeEstoque/Controllers/CadastroController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControleDeEstoque.Controllers
+{
+    public class CadastroController : Controller
+    {
+        // GET: Cadastro
+        public ActionResult GrupoPedido()
+        {
+            return View();
+        }
+        public ActionResult MarcaProduto()
+        {
+            return View();
+        }
+        public ActionResult LocaisProduto()
+        {
+            return View();
+        }
+        public ActionResult UnidadesMedidas()
+        {
+            return View();
+        }
+        public ActionResult Produto()
+        {
+            return View();
+        }
+        public ActionResult Pais()
+        {
+            return View();
+        }
+        public ActionResult Estado()
+        {
+            return View();
+        }
+        public ActionResult Cidade()
+        {
+            return View();
+        }
+        public ActionResult Fornecedor()
+        {
+            return View();
+        }
+        public ActionResult PerfilUsuario()
+        {
+            return View();
+        }
+        public ActionResult Usuario()
+        {
+            return View();
+        }
+
+    }
+}
